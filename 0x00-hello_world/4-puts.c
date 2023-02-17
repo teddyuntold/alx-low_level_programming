c05ff0f2e9ae308a0179c21920b4836f269bd94c
Frederick Otu-Afro <frederickotuafro@gmail.com>
2023-02-17 07:47:13 -0800

    printing without using print function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/ bin / bash
 #include <stdio.h>
 
 /**
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(" \ " Programming is like building a multilingual puzzle ");
 	return (0);
 }
